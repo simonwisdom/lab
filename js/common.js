e569b58d16bd66bcaf4ebc564a29619e94133c01
Simon Wisdom <simonowisdom@gmail.com>
2025-01-07 14:37:51 +0100

    removed stylesheet init

--- js/common.js
+++ js/common.js
@@ -125,7 +125,6 @@ function addTwitterCardTags() {
   addAnalytics();
   initializeGlobalWaitForUmami();
   addTracking();
-  addStylesheet();
   addFooter();
   addMetadata();
   addOpenGraphTags();
